95345a56f68e2df05d2db1cdbd58cf3d8eea0249
agayevv7 <109365950+agayevv7@users.noreply.github.com>
2026-05-08 14:11:10 +0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import string
 import os
 
 # --- HƏDƏF ---
-TARGET_HOST = "https://az.wikipedia.org/wiki/Bak%C4%B1_Biznes_Universiteti"
+TARGET_HOST = “empro.az”
 TARGET_PORT = 443
 THREADS = 800 # Railway Paid üçün maksimal stabil güc
 
